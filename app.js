e7583222372f67be6a036b18e94c9582993d2837
venusian6 <135219555+venusian6@users.noreply.github.com>
2025-03-05 00:10:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,17 +10,15 @@ const cors = require('cors')
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Uncomment to log on successful connection
+         console.log("MongoDB Connection Successful")
+    })
+    .catch(err => {
+        console.log("Error connecting to MongoDB: ", err);
+    });
 
-mongoose.connect('mongodb+srv://harrypotter007007007007:abc@cluster0.c74zw.mongodb.net/superaData',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-      //  console.log("MongoDB Connection Successful")
-    }
-})
 
 var Schema = mongoose.Schema;
 
@@ -32,54 +30,53 @@ var dataSchema = new Schema({
     velocity: String,
     distance: String
 });
+
 var planetModel = mongoose.model('planets', dataSchema);
 
-
-
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
+app.post('/planet', async (req, res) => {
+    try {
+        const planetData = await planetModel.findOne({
+            id: req.body.id
+        }).exec(); // Using exec() for better control of query execution
+        if (!planetData) {
+            res.status(404).send("Planet not found! We only have 9 planets and a sun. Select a number from 0 - 9");
         } else {
             res.send(planetData);
         }
-    })
-})
+    } catch (err) {
+        res.status(500).send("Error in Planet Data");
+    }
+});
 
-app.get('/',   async (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
-
-app.get('/os',   function(req, res) {
+app.get('/os', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "os": OS.hostname(),
         "env": process.env.NODE_ENV
     });
-})
+});
 
-app.get('/live',   function(req, res) {
+app.get('/live', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "live"
     });
-})
+});
 
-app.get('/ready',   function(req, res) {
+app.get('/ready', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send({
         "status": "ready"
     });
-})
+});
 
 app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
-})
-
+    console.log("Server successfully running on port - " + 3000);
+});
 
 module.exports = app;
+
